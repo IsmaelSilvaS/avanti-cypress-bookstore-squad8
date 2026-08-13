6689b9ab727ca3189291ca41098220f2e6ada427
leandro-smb <leandrobcontato@gmail.com>
2026-08-12 21:04:43 -0300

    testando primeiro push para o github

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,7 @@
+// testando subir pro github
+describe('Login Tests', () => {
+    it('login valido')
+        cy.visit('https://example.cypress.io/__/#/specs/runner?file=cypress/e2e/spec.cy.js')
+        
+        cy.it('')
+})
