14711d04de83f5fa3a90bc4da3451519cd8adc1d
leandro-smb <leandrobcontato@gmail.com>
2026-08-18 21:48:08 -0300

    adicionando variaveis de ambiente no cypress.env

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,8 +4,8 @@ describe('Login Tests', () => {
     it('login válido', () => {
         cy.visit('https://demoqa.com/login')
 
-        cy.get('#userName').type('testadorqa')
-        cy.get('#password').type('Testador@123')
+        cy.get('#userName').type(Cypress.env('usuario_teste'))
+        cy.get('#password').type(Cypress.env('senha_teste'))
         cy.get('#login').click()
     })
 })
