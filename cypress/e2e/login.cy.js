301a78bee714883bb33197b589351ba7c18c6e9e
leandro-smb <leandrobcontato@gmail.com>
2026-08-19 21:06:47 -0300

    add: descricao dos testes de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,17 @@
+/* 
+
+CENÁRIO 02 - Login
+Autor: Leandro Bernardini
+Automação: Cypress
+
+Casos automatizados:
+1. CT01: Validar o login com todos os campos válidos
+2. CT02: Validar o login com o campo usuário inválido
+3. CT03: Validar o login com o campo senha inválido
+4. CT04: Validar o login com todos os campos inválidos
+
+*/
+
 describe('Login Tests', () => {
     it('Todos os campos válidos', () => {
         const credenciais = Cypress.env('todos_campos_validos')
